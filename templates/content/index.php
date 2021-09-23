4d21bb16e60a1a621296445d4a95a94b5b564d79
Teczo <91040878+Teczo@users.noreply.github.com>
2021-09-23 12:59:26 +0800

    Add files via upload

---
+++ templates/content/index.php
@@ -0,0 +1,8 @@
+<h1>Hello world</h1>
+<button type="button"  onclick=myFunction() >Click Me!</button>
+<h1 id ="test" ></h1>
+<script>
+function myFunction() {
+  document.getElementById("test").innerHTML = "YOU CLICKED ME!";
+}
+</script>
